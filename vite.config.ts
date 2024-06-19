ea29133c74ec05bf502f7b3b6eb5e7c26cd215af
JiHyeon Lee <justjh30@gmail.com>
2024-06-20 08:09:53 +0900

    ✅ 테스트 환경 구축 및 일렉트론 관련 패키지 삭제 (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,18 @@
-import { defineConfig } from "vite";
-import path from "path";
-import electron from "vite-plugin-electron/simple";
+import { InlineConfig, UserConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: "electron/main.ts",
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, "electron/preload.ts"),
-      },
-      // Ployfill the Electron and Node.js API for Renderer process.
-      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer:
-        process.env.NODE_ENV === "test"
-          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-            undefined
-          : {},
-    }),
-  ],
-});
+  plugins: [react()],
+  test: {
+    //전역 변수 사용 허용
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/config/testSetup.js",
+    include: ["**/*.test.ts?(x)"],
+  },
+} as VitestConfigExport);
