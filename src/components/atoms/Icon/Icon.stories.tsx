370bfb91e05b17d2310572dd2337f2db4ed5341f
버그 하우스 <alsrb4171@gmail.com>
2024-07-08 13:47:05 +0900

    Duck 8  UI buttons (#10)
    * 백업용 커밋
    
    * 아이콘, 텍스트 버튼 제작
    
    * prettier 적용 및 코드리뷰 바탕으로 코드 수정

---
+++ src/components/atoms/Icon/Icon.stories.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Icon from "./Icon";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Icon> = {
+  title: "components/atoms/Icon",
+  component: Icon,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Icon>;
+
+// 아이콘이 보이지 않아 테스트용 검정 박스
+const DarkBox = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: black;
+`;
+
+export function Default({ args }: Story) {
+  return <Icon name={args?.name || "message"} alt={args?.alt || "못찾을 때"} />;
+}
+
+export function Dark({ args }: Story) {
+  return (
+    <DarkBox>
+      <Icon name={args?.name || "message"} alt={args?.alt || "못찾을 때"} />
+    </DarkBox>
+  );
+}
--- src/style/TypoStyle.tsx
+++ src/style/TypoStyle.tsx
@@ -7,7 +7,9 @@ type typeTypoStyle = keyof typeTypoKeys;
 
 export default function createTypoStyle(varTypo: typeTypoStyle) {
   const value = global.typo[varTypo].value;
+  // 글로벌 폰트 작동 안하는중
   return css`
+    font-family: ${value.fontFamily};
     font-size: ${value.fontSize + "px"};
     line-height: ${value.lineHeight + "px"};
     font-weight: ${value.fontWeight};
