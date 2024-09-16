aa4d76f51dc34e6e0b1a5de03941ef73205382fa
Sleepingoff <justjh30@gmail.com>
2024-09-16 18:52:59 +0900

    Icon 아이콘 경로 수정

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -1,5 +1,7 @@
-type Props = { name: string; alt: string };
+import Img from "../Img/Img";
 
-export default function Icon({ name, alt }: Props) {
-  return <img src={`/icons/${name}.svg`} alt={alt} width={24} height={24} />;
+export interface typeIconProps { src: string; alt: string; }
+
+export default function Icon({ src, alt }: typeIconProps) {
+  return <Img src={src} alt={alt} width={24} height={24} />;
 }
