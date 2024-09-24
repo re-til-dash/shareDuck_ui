49510333668ccbfb43da389596d0df102eaaf7e4
sunah <1004_sunah@naver.com>
2024-09-24 21:47:43 +0900

    Toast 개발

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ export {default as Button} from './components/molecules/Button/Button';
 export {default as FloatingButton} from './components/molecules/FloatingButton/FloatingButton';
 export {default as Details} from './components/molecules/Details/Details';
 export {default as Tag} from './components/molecules/Tag/Tag';
+export {default as Toast} from './components/molecules/Toast/Toast';
 export {default as Profile} from './components/molecules/Profile/Profile';
 
 
